3d82f74512515938d6387c9aa4eac9c4d79ab17a
Peter Luong <peter.luong678@gmail.com>
2024-05-19 22:20:29 +1000

    add error messages for empty and invalid searches

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,10 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:publishable_api_key],
       endpoint: 'https://cloud.iexapis.com/v1')
+      begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name , last_price: client.quote(ticker_symbol).latest_price)
+      rescue => exception
+        return nil
+      end
   end
 end
